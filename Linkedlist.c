3d9a7faada230a0eeb5516443e8027cd4dce8472
Nen Pachani <nenpanchani@gmail.com>
2025-07-15 17:36:52 +0530

    Refactor count_of_nodes function and remove redundant print statements

--- Linkedlist.c
+++ Linkedlist.c
@@ -6,6 +6,23 @@ struct Node {
     int data;
     struct Node* next;
 };
+
+int count_of_nodes(struct Node* head) {
+    int count =0;
+    if (head == NULL){
+        printf("List is empty\n");
+        return count;
+    }
+    struct Node *ptr = NULL;
+    ptr = head;
+    while(ptr != NULL){
+        count++;
+        ptr = ptr->next;
+    }
+    printf("Count of nodes in the list: %d\n", count);
+    return count;
+
+}
 // Main function to test the list
 int main() {
     struct Node * head =malloc(sizeof(struct Node));
@@ -23,9 +40,7 @@ int main() {
 
     head->next->next = current;
 
-    printf("%d\n",head->data);
-    printf("%d\n",head->next->data);
-    printf("%d\n",head->next->next->data);
+    count_of_nodes(head);
 
     return 0;
 }
